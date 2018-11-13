2fd3aa9cc6af8b8f86b676111b470f7803ef8444
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-13 17:27:31 +1100

    centerCardComponent: Fix column layout; fix CSS class names

--- src/components/CenterCard/index.js
+++ src/components/CenterCard/index.js
@@ -3,8 +3,8 @@ import './style.css';
 class CenterCard extends Component {
     render () {
         return (
-            <div className="login-container login-grid valign-wrapper row">
-                <div className="col login-container login-grid">
+            <div className="center-card-container login-grid valign-wrapper row">
+                <div className="col center-card-grid">
                     <div className="card white darken-1">
                         {this.props.children}
                     </div>
