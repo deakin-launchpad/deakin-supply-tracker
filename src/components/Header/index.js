da9770cfe4a08684a40d30ddab46531d81b051eb
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-14 09:47:37 +1100

    headerComponent: Fix indentation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import "./style.css";
 
 class Header extends Component {
-    render() {
-        return (
-            <nav className="black">
-                <div className="nav-wrapper">
-                    <a href="/" className="brand-logo">Supply Chain</a>
-                    <ul id="nav-mobile" className="right">
-                        <li><a href="/user/login">Login</a></li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+  render() {
+      return (
+          <nav className="black">
+              <div className="nav-wrapper">
+                  <a href="/" className="brand-logo">Supply Chain</a>
+                  <ul id="nav-mobile" className="right">
+                      <li><a href="/user/login">Login</a></li>
+                  </ul>
+              </div>
+          </nav>
+      );
+  }
 }
 
 export default Header;
