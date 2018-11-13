9f0b04990732db312b0bf4bbd16d7fbf1bf7672b
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-13 13:27:52 +1100

    userView: Add user view for all 4 actors

---
+++ src/views/User/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./style.css";
+
+class User extends Component {
+    render() {
+        return (
+            <div className="red-text">
+
+            </div>
+        );
+    }
+}
+
+export default User;
