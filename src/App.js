e5e5e33bc1002ba25b2133a761ccc641bc6d582a
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-13 13:32:16 +1100

    appJS: Correct materialize imports; app specific changes 1. Add app layout 2. Add routes for actors and world 3. Specify landingPage as root URL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
+import SupplyChainLandingPage from 'views/SupplyChainLandingPage';
+import User from 'views/User';
 import './App.css';
+import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css'
+import Header from "components/Header";
+import Footer from "components/Footer";
+import World from "views/World";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
-        </header>
+          <div className="blank-header">
+              <Header/>
+          </div>
+          <div className="blank-body">
+              <Switch>
+                  <Route
+                      exact path='/'
+                      component={SupplyChainLandingPage}
+                  />
+                  <Route
+                      path={['/farmer', '/exporter', '/importer', '/consumer']}
+                      component={User}
+                  />
+                  <Route
+                      path='/world'
+                      component={World}
+                  />
+
+              </Switch>
+          </div>
+          <div className="blank-footer">
+              <Footer/>
+          </div>
+
       </div>
     );
   }
