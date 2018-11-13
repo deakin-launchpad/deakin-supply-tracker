2c84c21eb1be29e20dabeac9c5d199a7f5432aec
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-13 13:19:07 +1100

    footerComponent: Add app specific changes 1. Actor based footer color change 2. World button 3. Items count button

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import "./style.css";
+import FlatButton from "components/FlatButton";
 
 class Footer extends Component {
+    changeDivClass() {
+        let currPath = window.location.pathname;
+        console.log(currPath);
+        if (currPath === "/") {
+            return "";
+        } else if (currPath === "/farmer") {
+            return "farmer";
+        } else if (currPath === "/exporter") {
+            return "exporter";
+        } else if (currPath === "/importer") {
+            return "importer";
+        } else if (currPath === "/consumer") {
+            return "consumer";
+        }
+    };
+
+    rightBoxShouldShow() {
+        if (this.changeDivClass() === "/") {
+            console.log('if entered');
+            return "footer-right-gone";
+        } else {
+            console.log('else entered');
+            return "footer-right-box";
+        }
+    }
     render() {
         return (
-            <div className="footer">
-            Change Footer/index.js!
+            <div className={"footer footer-container " + this.changeDivClass()}>
+                <div className="footer-left-box">
+                    <div className="nav-wrapper">
+                        <ul id="nav-mobile" className="left">
+                            <li><FlatButton href={"/world"} text={"WORLD"}/></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={this.rightBoxShouldShow()}>
+                    <div className="footer-right-box-item">Items </div>
+                    <div className="footer-right-box-item"><FlatButton text=""/></div>
+                </div>
             </div>
         );
     }
