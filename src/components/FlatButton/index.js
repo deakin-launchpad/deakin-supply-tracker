17362fe3aaebe0cc8e3f6d98f0961fe361143e8d
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-14 11:04:34 +1100

    flatButton: Fix indentation

--- src/components/FlatButton/index.js
+++ src/components/FlatButton/index.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './style.css';
 class FlatButton extends Component {
-    render() {
-        return (
-            <a 
-              href={this.props.href} 
-              className="black waves-effect waves-light btn-flat white-text"
-              onClick={() => this.props.onClick()}
-            >
-              {this.props.text}
-            </a>
-        );
-    }
+  render() {
+    return (
+      <a 
+        href={this.props.href} 
+        className="black waves-effect waves-light btn-flat white-text"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.text}
+      </a>
+    );
+  }
 }
 
 export default FlatButton;
