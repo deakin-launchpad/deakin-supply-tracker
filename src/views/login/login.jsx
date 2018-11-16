a833879bbdf786b02f64d3268d11b12c6e42de43
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-16 13:10:11 +1100

    loginComponent: Remove useless imports, comments; Rename variables

--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import importedApi from 'helpers/api.js';
-import ImportedAppHelper from "helpers/AppHelper.js";
 const API = new importedApi();
-const AppHelper = new ImportedAppHelper();
 
 class Login extends Component {
   constructor(props) {
@@ -18,50 +15,38 @@ class Login extends Component {
   }
 
   handleEmailChange=(e)=>{
-      console.log(e.target.id)
-      this.setState({
-        emailId: e.target.value
-      });
+    console.log(e.target.id)
+    this.setState({
+      emailId: e.target.value
+    });
   }
 
   handlePasswordChange=(e)=>{
-    //console.log(e.target.id)
     this.setState({
       password: e.target.value
     });
   }
 
- 
-
   performLogin = () => {
-   // console.log('login state',this.props.user.user.isLoggedIn)
-   
-    API.loginUser(this.state,this.props.user.handler)
-    
-    
-
-    //this.props.user.user.handler('hello')
-    //API.loginUser(this.state);
+    API.loginUser(this.state,this.props.parentProps.parentStateHandler)
   }
 
   render() {
     return (
       <div className="Login">
         <h1>
-          {this.props.user.user.title}
+          {this.props.parentProps.parentState.title}
         </h1>
         <div className='row'>
-
           <div className='row'>
-                <div className='col s6 offset-s3'>
-                <input placeholder="Placeholder" id="email" type="email" className="validate" onChange={this.handleChange} />
-                <input placeholder="Placeholder" id="password" type="password" className="validate" onChange={this.handleChange} />
-                <a className="waves-effect waves-light btn" onClick={this.performLogin}><i className="material-icons left">cloud</i>Login</a>
-
-
-                </div>
+            <div className='col s6 offset-s3'>
+              <input placeholder="Email" id="email" type="email" className="validate" onChange={this.handleEmailChange} />
+              <input placeholder="Password" id="password" type="password" className="validate" onChange={this.handlePasswordChange} />
+              <a className="waves-effect waves-light btn" onClick={this.performLogin}>
+                <i className="material-icons left">cloud</i>Login
+              </a>
+            </div>
           </div>
-
         </div>
       </div>
     );
