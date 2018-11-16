2739f15f739217cf0caf0cadebb21b0a2db06a73
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-16 12:48:37 +1100

    mainComponent: Add actors, world paths; add actor specific login feature

--- src/views/main/main.jsx
+++ src/views/main/main.jsx
@@ -1,31 +1,41 @@
 import React, { Component } from 'react';
-import logo from 'images/deakin-logo.svg'
 import Login from 'views/login/login.jsx'
-import { Switch, Route , Redirect} from 'react-router-dom'
+import { Switch, Route, Redirect} from 'react-router-dom'
 import Farmer from 'views/farmer/farmer.jsx'
 import Importer from 'views/importer/importer.jsx'
 import Exporter from 'views/exporter/exporter.jsx'
 import Consumer from 'views/consumer/consumer.jsx'
-
+import World from 'views/world/world.jsx';
+import ImportedAppHelper from "helpers/AppHelper.js";
+const AppHelper = new ImportedAppHelper();
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    
-  }
-  
   render() {
-    //console.log('routes',this.props.user.loggedIn)
+    // console.log('routes',this.props)
     return (
       <div className="Main container">
        <Switch>
-            <Route exact path='/' render={ (props) =><Login {...props} user={this.props}/>}/>
+            <Route exact path='/' render={ (props) => (
+              // Next line is failsafe
+              // this.props.parentState.loggedIn || AppHelper.isUserLoggedIn() ? (<Login {...props} parentProps={this.props}/>) : (<Login {...props} parentProps={this.props}/>)
+              // Next line is what needs to be always working
+              this.props.parentState.loggedIn || AppHelper.isUserLoggedIn() !== 'false' ? (<Redirect to={'/' + AppHelper.getUserRole() }/>) : (<Login {...props} parentProps={this.props}/>)
+            )}/>
             <Route exact path='/farmer' render={ (props) =>
-              (this.props.user.loggedIn?(<Farmer {...props} user={this.props}/>):(<Redirect to='/'/>))}
+              (this.props.parentState.loggedIn || AppHelper.isUserLoggedIn() && AppHelper.getUserRole() === 'farmer' ? (<Farmer {...props} parentProps={this.props}/>) : (<Redirect to='/'/>))}
             />
-            <Route exact path='/exporter' component={Exporter}/>
-            <Route exact path='/importer' component={Importer}/>
-       
+            <Route exact path='/exporter' render={ (props) =>
+              (this.props.parentState.loggedIn || AppHelper.isUserLoggedIn() && AppHelper.getUserRole() === 'exporter' ? (<Exporter {...props} parentProps={this.props}/>) : (<Redirect to='/'/>))}
+            />
+            <Route exact path='/importer' render={ (props) =>
+              (this.props.parentState.loggedIn || AppHelper.isUserLoggedIn() && AppHelper.getUserRole() === 'importer' ? (<Importer {...props} parentProps={this.props}/>) : (<Redirect to='/'/>))}
+            />
+            <Route exact path='/consumer' render={ (props) =>
+              (this.props.parentState.loggedIn || AppHelper.isUserLoggedIn() && AppHelper.getUserRole() === 'consumer' ? (<Consumer {...props} parentProps={this.props}/>) : (<Redirect to='/'/>))}
+            />
+            <Route exact path='/world' render={ (props) =>
+              <World parentProps={this.props}/> } //Renamed user to parentProps in world.jsx as well
+            />
             
         </Switch>
       </div>
