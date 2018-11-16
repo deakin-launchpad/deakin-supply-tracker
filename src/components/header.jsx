2ecda502ee284219c205dba6a4c98b0f49ca267e
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-16 12:51:49 +1100

    headerComponent: Remove useless constructor, import; Change style Remove hide-on-med-and-down from logout button to let it stay when view is shrinked horizontally.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
 import ImportedAppHelper from "helpers/AppHelper.js";
 const AppHelper = new ImportedAppHelper();
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  logout=()=>{
-    
+  logout = () => {
     AppHelper.logoutUser(this.props.logout)
   }
   render() {
 
-    console.log('props',this.props)
+    // console.log('props',this.props)
     return (
       <div className="navbar-fixed">
         <nav>
           <div className="nav-wrapper">
             <a href="#!" className="brand-logo">{this.props.title}</a>
-            <ul className="right hide-on-med-and-down">
+            <ul className="right">
              <li> <a onClick={this.logout } className="waves-effect waves-light btn">Logout</a></li>
             </ul>
           </div>
