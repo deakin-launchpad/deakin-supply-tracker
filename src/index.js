5fb557533b0562160f96fd55598f255813ce07e7
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-19 22:31:46 +1100

    indexJS: Remove useless import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 //import { BrowserRouter } from 'react-router-dom'
 import BrowserRouter from 'helpers/router';
-import { render } from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
