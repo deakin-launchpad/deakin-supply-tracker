b371ecae0a616bb40ff3f332a105c5672e9fa91b
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-23 10:40:19 +1100

    login: Add spacing in reducer

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -17,21 +17,26 @@ const login = (state = {loading: false, loggedIn: false, userRole: ''}, action) 
     case REQUEST_LOGIN_SUCCESS:
     case REQUEST_ACCESS_TOKEN_LOGIN_SUCCESS:
       return {...state, loading: false, loggedIn: true}
+
     case REQUEST_LOGIN_FAIL:
     case REQUEST_ACCESS_TOKEN_LOGIN_FAIL:
       return {...state, loading: false, loggedIn: false}
 
+
     case SET_USER_ROLE:
       return {...state, userRole: action.userRole}
-    
+
+
     case REQUEST_LOGOUT:
       return {...state, loading: false, loggedIn: false, userRole: ''}
- 
+
     case REQUEST_LOGOUT_SUCCESS:
       return {...state, loading: false, loggedIn: false}
+
     case REQUEST_LOGOUT_FAIL:
       return {...state, loading: false, loggedIn: true}
-    
+
+
     default:
       return {loading: false, loggedIn: false, userRole: ''}
   }
