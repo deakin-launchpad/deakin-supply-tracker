606bd42822fc3c51bf12b55ea96deaa183ff47cc
Akash Agarwal <akashaggarwal777@gmail.com>
2018-11-23 10:42:22 +1100

    login: Cleanup reducer actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,6 @@ export const REQUEST_LOGIN = "REQUEST_LOGIN"
 export const REQUEST_LOGOUT = "REQUEST_LOGOUT"
 export const REQUEST_ACCESS_TOKEN_LOGIN = "REQUEST_ACCESS_TOKEN_LOGIN"
 export const SET_USER_ROLE = "SET_USER_ROLE"
-export const RESPONSE_LOGIN = "SUCCESS_LOGIN"
-export const RESPONSE_LOGOUT = "SUCCESS_LOGOUT"
 
 export const requestLogin = (data) => ({
   type: REQUEST_LOGIN,
@@ -39,14 +37,4 @@ export const requestLogout = () => ({
 export const setUserRole = (userRole) => ({
   type: SET_USER_ROLE,
   userRole: userRole
-})
-
-export const responseLogin = (response) => ({
-  type: RESPONSE_LOGIN,
-  status: response.status
-})
-
-export const responseLogout = (response) => ({
-  type: RESPONSE_LOGOUT,
-  status: response.status
 })
