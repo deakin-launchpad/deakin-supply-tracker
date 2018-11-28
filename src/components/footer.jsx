dc10f925efa2ba7c9d15e83a8b41c799a3ff9866
Nirav Bhimani <niravbhimani53@gmail.com>
2018-11-28 11:49:06 +1100

    footerComponent: Fix footer tags

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,11 +4,13 @@ class Footer extends Component {
   render() {
     return (
       <footer className="Footer page-footer">
-        <div className="footer-copyright">
-          <div className="container">
-            <a href="/world" className="waves-effect waves-light btn">World State</a>
-            Supplies {this.props.worldSupplies}
-          </div>
+        <div className = "row">
+            <h5>Supplies {this.props.worldSupplies}</h5>
+        </div>
+        <div className = "footer-copyright">
+            <div className = "container">
+              <a href="/world" className="waves-effect waves-light btn center">World State</a> 
+            </div>
         </div>
       </footer>
     );
