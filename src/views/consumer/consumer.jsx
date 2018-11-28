5dd388843abe3223e6ae96f5fff704b3db14996d
Nirav Bhimani <niravbhimani53@gmail.com>
2018-11-28 11:51:06 +1100

    consumerComponent: Add new tags for css

--- src/views/consumer/consumer.jsx
+++ src/views/consumer/consumer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import API from 'helpers/api.js';
+import bodyLogo from "images/farmer.png";
 
 class Consumer extends Component {
   buyItems = () => {
@@ -8,15 +9,44 @@ class Consumer extends Component {
 
   render() {
     return (
-      <div className="Consumer">
-        <h1>
+      <div className="Consumer bodyDiv">
+        <p className="titleText">
           Consumer
-        </h1>
-        <h2>Supplies</h2>
-        <h3>At Importer {this.props.parentProps.parentState.worldState.importer}</h3>
-        <h3>Warehouse {this.props.parentProps.parentState.worldState.consumer}</h3>
-        <a className="waves-effect waves-light btn" href="#!" onClick={this.buyItems}>Buy From Importer</a>
-        <a className="waves-effect waves-light btn" href="#!">Consume Supplies</a>
+        </p>
+        <img className="bodyImg" src={bodyLogo} alt="Farmer"/>
+        <div className="mainDiv">
+          <div className="row">
+            <div className="col s12 boldFont">
+              Supplies
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s9">
+              <span className="text1"> At Importer </span> <br/>
+              <span className="text2"> Supplies currently at the importer</span>
+            </div>
+            <div className="col s3 ">
+              <div className="valueBox"> 
+                {this.props.parentProps.parentState.worldState.importer}
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s9">
+              <span className="text1"> Warehouse </span> <br />
+              <span className="text2"> Supplies currently in Warehouse </span>
+            </div>
+            <div className="col s3">
+              <div className="valueBox">
+                {this.props.parentProps.parentState.worldState.consumer}
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <button className="waves-effect submitBtn waves-light btn" onClick={this.buyItems}>Buy From Importer</button>
+            <button className="waves-effect submitBtn waves-light btn">Consume Supplies</button>
+          </div>
+        </div>
       </div>
     );
   }
